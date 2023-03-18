ba7b9cd5e2fdef31abb918225d62e1f9c78dee63
Sungjoo Jeon <sungjoojeon@berkeley.edu>
2023-03-17 17:30:42 -0700

    more stickers + layout stuff

--- landing.js
+++ landing.js
@@ -1,15 +1,2 @@
 console.log("If you're seeing this, then that means you're snooping around my website. "
 + "Please hire me <3");
-
-/*
-//const ring = document.querySelector(".ring");
-var spine = document.querySelector(".spine");
-
-for (let i=0; i<10; i++) {
-    var ring = document.createElement("img");
-    ring.src = "images/spine.svg";
-    ring.setAttribute("style", "height:4vh");
-
-    spine.appendChild()
-}
-*/
